91d0355034c439232482bb179bb7561539651e27
oleg <o1egov@mail.ru>
2025-11-30 18:47:45 +0700

    нанесение первых данных на диаграмму.

---
+++ 111.py
@@ -0,0 +1,2 @@
+from matplotlib import pyplot as plt
+print(dir(plt.style.use))
--- sitka_highs.py
+++ sitka_highs.py
@@ -1,4 +1,6 @@
 import csv
+from matplotlib import pyplot as plt
+from pexpect import which
 
 filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
@@ -11,3 +13,16 @@ with open(filename) as f:
         high = int(row[5])
         highs.append(high)
 print(highs)
+
+# нанесение данных на диаграмму
+plt.style.use('classic')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+# форматирование диаграммы
+plt.title('Daile high temperatures, July 2018',fontsize=24)
+plt.xlabel('',fontsize=16)
+plt.ylabel('Temperature (F)', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
