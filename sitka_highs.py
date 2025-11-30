2d83d0bb5b810bd7a328e380c5c7108f52fd2aa8
oleg <o1egov@mail.ru>
2025-11-30 18:29:02 +0700

    извлечение и чтение данных

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,12 +1,13 @@
 import csv
 
 filename = 'data/sitka_weather_07-2018_simple.csv'
-filename2 = 'data/sample.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
-with open(filename2) as f2:
-    a = csv.reader(f2)
-    a_row = next(f2)
-    print(a_row)
+
+    # чтение максимальных температур
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+print(highs)
