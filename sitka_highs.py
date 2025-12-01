4688d36c59d0e8805416f61607dcd8d4a0ff39a2
oleg <o1egov@mail.ru>
2025-12-01 18:08:06 +0700

    диаграмма ежедневных осадков

--- sitka_highs.py
+++ sitka_highs.py
@@ -6,29 +6,36 @@ filename = 'data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
+    for k, column_header in enumerate(header_row):
+        print(k, column_header)
 
     # чтение дат и максимальных температур
-    dates, highs, lows = [], [], []
+    dates, highs, lows, precips = [], [], [],[]
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
         low = int(row[6])
+        precip = float(row[3])
         dates.append(current_date)
         highs.append(high)
         lows.append(low)
+        precips.append(precip)
 
 # нанесение данных на диаграмму
 plt.style.use('classic')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c='red', alpha=0.5)
-ax.plot(dates, lows, c='blue', alpha=0.5)
-plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+#ax.plot(dates, highs, c='red', alpha=0.5)
+#ax.plot(dates, lows, c='blue', alpha=0.5)
+ax.plot(dates, precips, c='green', alpha=0.5)
+#plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # форматирование диаграммы
-plt.title('Daile high and low temperatures - 2018', fontsize=24)
+#plt.title('Daile high and low temperatures - 2018', fontsize=24)
+plt.title('The amount of daily the amount of \ndaily precipitation Sitka - 2018', fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
-plt.ylabel('Temperature (F)', fontsize=16)
+#plt.ylabel('Temperature (F)', fontsize=16)
+plt.ylabel('Precipitation (mm)', fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
 plt.show()
