e6d0aaf59207834b919118843c879ee07ddd3762
oleg <o1egov@mail.ru>
2025-12-03 18:26:02 +0700

    извлечение магнитуд

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -3,6 +3,14 @@ filename = 'data/eq_data_1_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
+all_eq_dicts = all_eq_data['features']
+mags = []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+print(len(all_eq_dicts))
+print(mags[:10])
+
 readable_file = 'data/readable_eq_data.json'
 with open(readable_file,'w') as f:
     json.dump(all_eq_data, f, indent=4)
