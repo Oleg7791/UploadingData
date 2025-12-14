92134b79fe02cd446ffe45ea659f2ee089e0cf35
oleg <o1egov@mail.ru>
2025-12-14 19:08:31 +0700

    сводка самых популярных репозиториев

--- python_repos.py
+++ python_repos.py
@@ -14,20 +14,18 @@ print(f'Total repositories: {response_dict['total_count']}')
 repo_dicts = response_dict['items']
 print(f'Repositories returned: {len(repo_dicts)}')
 
-# анализ первого репозитория
-repo_dict = repo_dicts[0]
-
-print('\nSelected information about first repository:')
-print(f'Name: {repo_dict['name']}')
-print(f'Owner: {repo_dict['owner']['login']}')
-print(f'Stars: {repo_dict['stargazers_count']}')
-print(f'Repository: {repo_dict['html_url']}')
-print(f'Created: {repo_dict['created_at']}')
-print(f'Updated: {repo_dict['updated_at']}')
-print(f'Description: {repo_dict['description']}')
-print(f'\nKeys: {len(repo_dict)}')
-for key in sorted(repo_dict.keys()):
-    print(key)
+print('\nSelected information about each repository:')
+for repo_dict in repo_dicts:
+    print(f'Name: {repo_dict['name']}')
+    print(f'Owner: {repo_dict['owner']['login']}')
+    print(f'Stars: {repo_dict['stargazers_count']}')
+    print(f'Repository: {repo_dict['html_url']}')
+    print(f'Created: {repo_dict['created_at']}')
+    print(f'Updated: {repo_dict['updated_at']}')
+    print(f'Description: {repo_dict['description']}')
+# print(f'\nKeys: {len(repo_dict)}')
+# for key in sorted(repo_dict.keys()):
+#     print(key)
 
 # обработка результатов
 print(response_dict.keys())
