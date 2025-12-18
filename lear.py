db1a79e454a2635b829fed6861874945cfc7d73f
oleg <o1egov@mail.ru>
2025-12-18 19:22:06 +0700

    API Hacker News использование вызова га сайте хакер ньюс

--- lear.py
+++ lear.py
@@ -1,9 +1,1 @@
-import requests
-
-#Make an API call and store the response.
-url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
-headers = {'Accept': 'application/vnd.github.v3+json'}
-r = requests.get(url, headers=headers)
-response_dict = r.json()
-print(f"Status code: {r.status_code}")
-print(response_dict.keys())
+print('hello world')
